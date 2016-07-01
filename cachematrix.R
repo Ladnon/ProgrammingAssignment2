cdd5cede93a797f02a170c7458ba9fec88529866
Martin Nondal Mason <nondalmason@gmail.com>
2016-07-01 23:24:48 +0200

    This is my attempt at the Week 3 Assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions take a matrix, and reproduce the original matrix or the inverted version.
 
-## Write a short comment describing this function
+## This function takes the matrix as input and produces the inverse, making both the original
+##and the inverse available in a list.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        i <- NULL
+        set <- function(y) {
+                x <<- y
+                i <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(solve) i <<- solve
+        getinverse <- function() i
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## This function checks if there has already been calculated a inverse and gives you the 
+##result if it has already been calculated. If not it will calculate and return it.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x,...) {
+        i <- x$getinverse()
+        if(!is.null(i)) {
+                message("getting cached data")
+                return(i)
+        }
+        data <- x$get()
+        i <- solve(data,...)
+        x$setinverse(i)
+        i
+        
 }
